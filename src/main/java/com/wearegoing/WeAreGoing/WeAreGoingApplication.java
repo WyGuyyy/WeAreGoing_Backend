e8c22a6c7c800ef5d582257c2fc5dfe824c734d6
wmtowne <wmtowne@svsu.edu>
2021-03-11 20:33:20 -0500

    added reset amount thread

--- src/main/java/com/wearegoing/WeAreGoing/WeAreGoingApplication.java
+++ src/main/java/com/wearegoing/WeAreGoing/WeAreGoingApplication.java
@@ -3,82 +3,83 @@ package com.wearegoing.WeAreGoing;
 import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 
 import com.wearegoing.WeAreGoing.Info.InfoService;
 
 @SpringBootApplication
 public class WeAreGoingApplication {
-	
-	@Autowired
-	static InfoService infoService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeAreGoingApplication.class, args);
-		//startResetThread(); - this isnt working, autowire not working in different thread?
+		startResetThread();
+	}
+	
+	public static void startResetThread(){
+		
 	}
 
-	//START HERE NEXT TIME AND RUN RESET EVERY SATURDAY AT MIDNIGHT
-		public static void startResetThread() {
-			
-			Thread resetThread = new Thread(new Runnable() {
-			
-			public void run() {
-		       
-		        long msTillTimeout;
-				
-				while(true) {
-					
-					Calendar c = Calendar.getInstance();
-					
-					if(c.get(Calendar.DAY_OF_WEEK) == Calendar.THURSDAY && (c.get(Calendar.HOUR_OF_DAY) == 18 && c.get(Calendar.MINUTE) > 10)) {
-						System.out.println(c.get(Calendar.MINUTE));
-						System.out.println(c.get(Calendar.HOUR_OF_DAY));
-						infoService.resetAmount();
-						
-						while(c.get(Calendar.HOUR_OF_DAY) < 12) {
-							//Loop until out of inactive period
-						}
-					}
-				
-					/*setToNextSaturday(Calendar.SATURDAY, c);
-					msTillTimeout = (c.getTimeInMillis()-System.currentTimeMillis());
-					
-					try {
-						Thread.sleep(msTillTimeout);
-						
-						while(true) {
-							
-						}
-						
-					} catch (InterruptedException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}*/
-					
-					c = null;
-					
-				}
-				
-			}
-			
-			});
-		
-			resetThread.start();
-		}
-		
-		/*public static void setToNextSaturday(int dayOfWeekToSet, Calendar c) {
-				int currentDayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-				System.out.println(c.DAY_OF_MONTH);
-				System.out.println(currentDayOfWeek);
-				
-				while(currentDayOfWeek != dayOfWeekToSet) {
-					c.add(Calendar.DAY_OF_MONTH, 1);
-					currentDayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-				}
-				
-				System.out.println(c.DAY_OF_WEEK);
-		}*/
-		
+	@Bean
+	public TaskExecutor taskExecutor() {
+	    return new SimpleAsyncTaskExecutor(); // Or use another one of your liking
 	}
+	
+	@Bean
+	public CommandLineRunner schedulingRunner(TaskExecutor executor) {
+	    return new CommandLineRunner() {
+	    	
+	    	@Autowired
+	    	InfoService infoService;
+	    	
+	        public void run(String... args) throws Exception {
+	            executor.execute(new Runnable() {
+	    			
+	    			public void run() {
+	    		       
+	    		        long msTillTimeout;
+	    				
+	    				while(true) {
+	    					
+	    					Calendar c = Calendar.getInstance();
+
+	    					if(c.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY && c.get(Calendar.HOUR_OF_DAY) < 12) {
+
+	    						infoService.resetAmount();
+	    						
+	    						while(c.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY && c.get(Calendar.HOUR_OF_DAY) < 12) {
+	    							//Loop until out of inactive period
+	    						}
+	    					}
+	    				
+	    					/*setToNextSaturday(Calendar.SATURDAY, c);
+	    					msTillTimeout = (c.getTimeInMillis()-System.currentTimeMillis());
+	    					
+	    					try {
+	    						Thread.sleep(msTillTimeout);
+	    						
+	    						while(true) {
+	    							
+	    						}
+	    						
+	    					} catch (InterruptedException e1) {
+	    						// TODO Auto-generated catch block
+	    						e1.printStackTrace();
+	    					}*/
+	    					
+	    					c = null;
+	    					
+	    				}
+	    				
+	    			}
+	    			
+	    			});
+	        }
+	    };
+	}
+	
+}
